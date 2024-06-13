199a6851fea0bf9acbf0496f96fe072f3c7068ba
Andrew N. Smith <asmith@owlshares.com>
2024-06-13 13:32:43 -0400

    add some print statements to watcher

--- src/watcher.rs
+++ src/watcher.rs
@@ -5,6 +5,7 @@ use std::path::Path;
 use std::sync::Arc;
 
 pub async fn monitor_directory(path: &str, queue: Arc<RwLock<VecDeque<Event>>>) -> std::io::Result<()> {
+    println!("setting up directory watcher for path: {}", path);
     let inner_queue = queue.clone();
     let (tx, rx) = std::sync::mpsc::channel();
 
@@ -31,10 +32,13 @@ pub async fn monitor_directory(path: &str, queue: Arc<RwLock<VecDeque<Event>>>) 
 
     tokio::spawn(async move {
         while let Ok(event) = rx.recv() {
+            println!("Received event: {:?}", event);
             let mut guard = inner_queue.write().await;
             guard.push_back(event);
         }
     });
+
+    println!("Directory watcher setup complete");
 
     Ok(())
 }
